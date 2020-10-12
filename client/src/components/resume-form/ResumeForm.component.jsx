f1ed395d250bfde732efd2787f9b1f2bb6124da5
Florian <mesagouiffes@gmail.com>
2020-10-12 18:58:07 +0200

    [create:component] generate resume-form && resume-preview

---
+++ client/src/components/resume-form/ResumeForm.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./ResumeForm.scss";
+
+const ResumeForm = () => {
+  return (
+    <div>
+      <p>ResumeForm component</p>
+    </div>
+  );
+};
+
+export default ResumeForm;
---
+++ client/src/components/resume-preview/ResumePreview.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./ResumePreview.scss";
+
+const ResumePreview = () => {
+  return (
+    <div>
+      <p>ResumePreview component</p>
+    </div>
+  );
+};
+
+export default ResumePreview;
