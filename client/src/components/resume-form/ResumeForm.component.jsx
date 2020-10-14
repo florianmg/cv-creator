316a6218a2867b06efb49c235708cd3888f6201d
Florian <mesagouiffes@gmail.com>
2020-10-14 18:22:06 +0200

    [Resume] Add button submit

--- client/src/components/resume-form/ResumeForm.component.jsx
+++ client/src/components/resume-form/ResumeForm.component.jsx
@@ -1,38 +1,46 @@
 import React from "react";
+
 import Input from "../input";
+import Button from "../button";
 
 import "./ResumeForm.scss";
 
 const ResumeForm = ({ resumeLocalInfos, setResumeLocalInfos }) => {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(resumeLocalInfos);
+  };
   return (
     <div className="resume-form">
-      <p>ResumeForm component</p>
-      <Input
-        type="text"
-        label="Nom"
-        id="lastName"
-        value={resumeLocalInfos.lastName}
-        required={true}
-        onChange={(e) =>
-          setResumeLocalInfos({
-            ...resumeLocalInfos,
-            lastName: e.target.value,
-          })
-        }
-      />
-      <Input
-        type="text"
-        label="Prénom"
-        id="firstName"
-        value={resumeLocalInfos.firstName}
-        required={true}
-        onChange={(e) =>
-          setResumeLocalInfos({
-            ...resumeLocalInfos,
-            firstName: e.target.value,
-          })
-        }
-      />
+      <form onSubmit={handleFormSubmit}>
+        <Input
+          type="text"
+          label="Nom"
+          id="lastName"
+          value={resumeLocalInfos.lastName}
+          required={true}
+          onChange={(e) =>
+            setResumeLocalInfos({
+              ...resumeLocalInfos,
+              lastName: e.target.value,
+            })
+          }
+        />
+        <Input
+          type="text"
+          label="Prénom"
+          id="firstName"
+          value={resumeLocalInfos.firstName}
+          required={true}
+          onChange={(e) =>
+            setResumeLocalInfos({
+              ...resumeLocalInfos,
+              firstName: e.target.value,
+            })
+          }
+        />
+        <Button text="Enregistrer les modifications" />
+      </form>
     </div>
   );
 };
