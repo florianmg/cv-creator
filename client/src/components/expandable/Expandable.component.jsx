1082886b983649b91c3c6b5292fd4407107ab68f
Florian <mesagouiffes@gmail.com>
2020-10-14 18:21:08 +0200

    [create:component] Expandable

---
+++ client/src/components/expandable/Expandable.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Expandable.scss";
+
+const Expandable = () => {
+  return (
+    <div>
+      <p>Expandable component</p>
+    </div>
+  );
+};
+
+export default Expandable;
