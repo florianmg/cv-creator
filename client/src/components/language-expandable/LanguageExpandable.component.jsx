a10a92ad1b4817afd408b9a868d08b8a7f3c95dc
Florian <mesagouiffes@gmail.com>
2020-10-22 13:54:35 +0200

    generate language-expandable component

---
+++ client/src/components/language-expandable/LanguageExpandable.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./LanguageExpandable.scss";
+
+const LanguageExpandable = () => {
+  return (
+    <div>
+      <p>LanguageExpandable component</p>
+    </div>
+  );
+};
+
+export default LanguageExpandable;
---
+++ client/src/components/language-expandable/index.js
@@ -0,0 +1,1 @@
+export {default} from './LanguageExpandable.component';
