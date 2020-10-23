b94e57d5d3d751f7dd4aaedeba1b125d12f59193
Florian <mesagouiffes@gmail.com>
2020-10-23 15:19:59 +0200

    generate interest-expandable component

---
+++ client/src/components/interest-expandable/InterestExpandable.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./InterestExpandable.scss";
+
+const InterestExpandable = () => {
+  return (
+    <div>
+      <p>InterestExpandable component</p>
+    </div>
+  );
+};
+
+export default InterestExpandable;
