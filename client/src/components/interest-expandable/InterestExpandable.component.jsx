4923956f7214b940ab0595fc87ec466e1a5c81b2
Florian <mesagouiffes@gmail.com>
2020-10-24 13:04:29 +0200

    input to add interests

--- client/src/components/interest-expandable/InterestExpandable.component.jsx
+++ client/src/components/interest-expandable/InterestExpandable.component.jsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from "react";
+
+import Input from "../input";
 import Expandable from "../expandable";
 
 import "./InterestExpandable.scss";
 
 const InterestExpandable = ({ interests, updateInput, type }) => {
-  const [defaultList, setDefaultList] = useState([
+  const initialDefaultList = [
     "Lecture",
     "Sport",
     "Voyages",
     "Broderie",
     "Jeux-Vidéos",
-  ]);
+  ];
+  const [defaultList, setDefaultList] = useState(initialDefaultList);
   const [selectedList, setSelectedList] = useState(interests);
+  const [inputValue, setInputValue] = useState("");
 
   const handleAddItemToList = (itemName) => {
     if (selectedList.findIndex((item) => item.name === itemName) !== -1) return;
@@ -22,6 +26,7 @@ const InterestExpandable = ({ interests, updateInput, type }) => {
     };
 
     setSelectedList([...selectedList, itemToAdd]);
+    setInputValue("");
   };
 
   const handleRemoveItemFromList = (itemName) => {
@@ -31,13 +36,45 @@ const InterestExpandable = ({ interests, updateInput, type }) => {
     setSelectedList(newSelectedList);
   };
 
+  const refreshDefaultList = () => {
+    const refreshedDefaultList = initialDefaultList.filter((item) => {
+      return selectedList.findIndex((e) => e.name === item) === -1;
+    });
+    setDefaultList(refreshedDefaultList);
+  };
+
+  useEffect(() => {
+    if (inputValue === "") {
+      setDefaultList(initialDefaultList);
+      return;
+    }
+    const filteredList = initialDefaultList.filter((item) =>
+      item.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    setDefaultList(filteredList);
+  }, [inputValue]);
+
   useEffect(() => {
     updateInput(selectedList);
+    refreshDefaultList();
   }, [selectedList]);
 
   return (
     <div className="interest-expandable">
       <Expandable title="Intérêts">
+        <Input
+          label="Ajouter un intérêt"
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        {inputValue.length !== 0 && (
+          <div>
+            <p>{inputValue}</p>
+            <p onClick={() => handleAddItemToList(inputValue)}>-Ajouter-</p>
+          </div>
+        )}
+
         <ul>
           {defaultList.map((itemName, i) => (
             <li onClick={() => handleAddItemToList(itemName)} key={i}>
@@ -46,7 +83,7 @@ const InterestExpandable = ({ interests, updateInput, type }) => {
           ))}
         </ul>
 
-        <p>Mes intérest :</p>
+        <p className="interest-expandable__selected-list">Mes intérest :</p>
         <ul>
           {selectedList &&
             selectedList.map((item, i) => (
